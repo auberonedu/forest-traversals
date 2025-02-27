5ea940189b992f18ddb4537579902255ef267910
TavSingh <tavparsadsingh2@gmail.com>
2025-02-27 13:50:37 -0800

    Fixing some issues

--- src/TreeProblems.java
+++ src/TreeProblems.java
@@ -65,6 +65,15 @@ public class TreeProblems {
    5
    */
   public static <T> void postOrder(Map<T, List<T>> tree, T root) {
+    if (tree == null || !tree.containsKey(root));
+  
+    List<T> children = tree.get(root);
+
+    for(T child : children) {
+      postOrder(tree, child);
+    }  
+  
+    System.out.println(root);
   }
 
   /*
